3bc9d21c2b264b4c55a504dc43a7da20b9b5e418
Joseph Salvatori <joseph.salvatori@gmail.com>
2025-03-05 09:40:01 -0600

    link zork

---
+++ src/routes/(public)/zork/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+import { getContext, onMount } from "svelte";
+import { terminal } from "$lib/helpers/terminal";
+
+/** @type {import('./$types').PageProps} */
+let { data } = $props();
+
+let terminalCtx = getContext("terminalCtx");
+
+let playCmd = `play zork`;
+let commands = terminal.getOutput(playCmd);
+
+terminalCtx.currentPage = "zork";
+terminalCtx.currentPageCmd = playCmd;
+// terminalCtx.currentPageCommands = commands;
+</script>
